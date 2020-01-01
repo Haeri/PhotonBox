6b09c8af1b264b7916c71a6d47fc1329b9457fef
Haeri <Haeri@users.noreply.github.com>
2020-01-02 00:26:07 +0100

    Downgrade to vs2017 cause travis can't even

--- Tools/generate_project_win.bat
+++ Tools/generate_project_win.bat
@@ -3,7 +3,7 @@
 set oldpath=%cd% 
 cd "%~dp0"
 
-call "premake/win/premake5.exe" --file="../premake5.lua" vs2019
+call "premake/win/premake5.exe" --file="../premake5.lua" vs2017
 
 cd "%oldpath%"
 IF NOT "%1"=="-s" PAUSE
