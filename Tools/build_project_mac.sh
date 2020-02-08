5081eba03638fd8e0f3d24ce89d961884ca1fbee
Haeri <Haeri@users.noreply.github.com>
2020-02-08 14:53:01 +0100

    fixing release configuration
    - Added build scripts

---
+++ Tools/build_project_mac.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+oldpath=$(pwd)
+cd $(dirname "$0")
+
+if [ -z "$1" ] ; then
+	make config=release
+elif [ $1 == "Release"] ; then
+	make config=release
+elif [ $1 == "Debug"] ; then
+	make
+fi
+
+cd $oldpath
---
+++ Tools/build_project_win.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set oldpath=%cd% 
+cd "%~dp0"
+
+if [%1] == [] (
+	call MSBuild.exe PhotonBox.sln /p:Configuration=Release
+	goto end
+) 
+if %1 == "Release" (
+	call MSBuild.exe PhotonBox.sln /p:Configuration=Release
+	goto end
+)
+if %1 == "Debug" (
+	call MSBuild.exe PhotonBox.sln
+	goto end
+)
+
+:end 
+cd "%oldpath%"
+IF NOT "%2"=="-s" PAUSE
