cc3e89c519e023e1f04b54161767328d5ade3c60
Haeri <Haeri@users.noreply.github.com>
2020-02-08 15:21:13 +0100

    wrong path

--- Tools/build_project_mac.sh
+++ Tools/build_project_mac.sh
@@ -4,11 +4,11 @@ oldpath=$(pwd)
 cd $(dirname "$0")
 
 if [ -z "$1" ] ; then
-	make config=release
-elif [ $1 == "Release"] ; then
-	make config=release
-elif [ $1 == "Debug"] ; then
-	make
+	make .. config=release
+elif [ $1 == "Release" ] ; then
+	make .. config=release
+elif [ $1 == "Debug" ] ; then
+	make ..
 fi
 
 cd $oldpath
--- Tools/build_project_win.bat
+++ Tools/build_project_win.bat
@@ -4,15 +4,15 @@ set oldpath=%cd%
 cd "%~dp0"
 
 if [%1] == [] (
-	call MSBuild.exe PhotonBox.sln /p:Configuration=Release
+	call MSBuild.exe ../PhotonBox.sln /p:Configuration=Release
 	goto end
 ) 
 if %1 == "Release" (
-	call MSBuild.exe PhotonBox.sln /p:Configuration=Release
+	call MSBuild.exe ../PhotonBox.sln /p:Configuration=Release
 	goto end
 )
 if %1 == "Debug" (
-	call MSBuild.exe PhotonBox.sln
+	call MSBuild.exe ../PhotonBox.sln
 	goto end
 )
 
