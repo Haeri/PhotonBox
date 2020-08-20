57c94f1c4fa678b04bbec690648770a70f2e1937
Haeri <luka.l@gmx.ch>
2020-08-20 17:13:41 +0200

    ci fix

--- tools/generate_project_mac.sh
+++ tools/generate_project_mac.sh
@@ -10,4 +10,10 @@ cd build
 
 cmake .. -DVCPKG_TARGET_TRIPLET=x64-osx -DCMAKE_TOOLCHAIN_FILE="../extern/vcpkg/scripts/buildsystems/vcpkg.cmake"
 
+err=$?
+
+if [ $err -ne 0 ] ; then
+	exit $err
+fi
+
 cd $oldpath
