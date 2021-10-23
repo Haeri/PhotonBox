52bc8ee581e167b0f60740f6a33512a4ad3154a7
Haeri <luka.l@protonmail.ch>
2021-10-23 23:06:05 +0200

    staging 2

--- scripts/generate_project_linux.sh
+++ scripts/generate_project_linux.sh
@@ -26,7 +26,7 @@ if [ "$1" = "-static" ]; then
 	build_type="-DBUILD_SHARED_LIBS=OFF"
 fi
 
-cmake .. -DVCPKG_TARGET_TRIPLET=x64-linux -DVCPKG_OVERLAY_PORTS=$root_path"/external/custom-ports" -DCMAKE_TOOLCHAIN_FILE=$root_path"/external/vcpkg/scripts/buildsystems/vcpkg.cmake" $build_type
+cmake .. -DVCPKG_TARGET_TRIPLET=x64-linux -DCMAKE_TOOLCHAIN_FILE=$root_path"/external/vcpkg/scripts/buildsystems/vcpkg.cmake" $build_type
 err=$?
 
 if [ $err -ne 0 ]; then
--- scripts/generate_project_win.bat
+++ scripts/generate_project_win.bat
@@ -35,7 +35,6 @@ if "%1" == "-static" (
 
 cmake .. ^
 	-DVCPKG_TARGET_TRIPLET="%triplet_value%" ^
-	-DVCPKG_OVERLAY_PORTS="%root_path%\external\custom-ports" ^
 	-DCMAKE_TOOLCHAIN_FILE="%root_path%\external\vcpkg\scripts\buildsystems\vcpkg.cmake" %build_type%
 set /a "err=%err%+%errorlevel%"
 
